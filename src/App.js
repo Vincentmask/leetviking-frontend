98dce301e3d3e260d02bab6a4e03b813f635c26d
haosheng Liang <haosheng @pdx.edu>
2024-07-14 20:00:12 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import MenuBar from "./components/MenuBar";
+import HomePage from "./views/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <MenuBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/views/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class HomePage extends React.Component {
+    state = {
+        courses: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> This is Home</h1>
+
+            </div>
+        )
+    }
+}
+
+export default HomePage;
